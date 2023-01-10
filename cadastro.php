bc330955cebcdcf0935f97e7e62f9d58430b568a
Gustavo Santos <guuhsrdeker@gmail.com>
2023-01-10 17:01:49 -0300

    Cadastro e login efetivados

--- cadastro.php
+++ cadastro.php
@@ -1,6 +1,51 @@
 <?php
 include("config/conexao.php");
 
+//Verificar se a postagem existe de acordo com os campos
+if(isset($_POST['nome']) && isset($_POST['email']) && isset($_POST['senha'])){
+    //Verificar se todos os campos foram preenchidos
+    if(empty($_POST['nome']) or empty($_POST['email']) or empty($_POST['senha'])){
+        $erro_geral = "Todos os campos são obrigatórios!";
+    }else{
+        //Receber e limpar dados do post
+        $nome = limpaPost($_POST['nome']);
+        $email = limpaPost($_POST['email']);
+        $senha = limpaPost($_POST['senha']);
+        $senha_cript = sha1($senha);
+
+        //Verificação individual de campos
+        //Nome
+        if (!preg_match("/^[a-zA-Z-' ]*$/",$nome)) {
+            $erro_nome = "Apenas letras e espaços";
+        }
+        //Email
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $erro_email = "Fomato de e-mail inválido";
+        }
+        //Senha
+        if(strlen($senha) < 6){
+            $erro_senha = "Senha devo ter 6 caracteres ou mais!";
+        }
+
+        if(!isset($erro_geral) && !isset($erro_nome) && !isset($erro_email) && !isset($erro_senha)){
+            //Verificar se o usuário já está cadastrado
+            $sql = $pdo->prepare("SELECT * FROM user WHERE user_email=? ");
+            $sql->execute(array($email));
+            $usuario = $sql->fetch();
+
+            if(!$usuario){
+                $nivel ="";
+                $token="";
+                $sql = $pdo->prepare("INSERT INTO user VALUES (null,?,?,?,?,?)");
+                $sql->execute(array($nome, $email, $senha_cript,$nivel, $token));
+                header('Location: index.php');
+            }else{
+                $erro_geral = "E-mail já cadastrado";
+            }
+        }
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -18,16 +63,16 @@ include("config/conexao.php");
             <form class="form-login" method="POST">
                 <h1 class="h1-login">Cadastro</h1>
 
-                <?php //Erro login
-                    if(isset($erro_login)){?>
+                <?php //Erro $erro_geral
+                    if(isset($erro_geral)){?>
                         <div class="erro-geral animate__animated animate__headShake">
-                            <?php echo $erro_login; ?>
+                            <?php echo $erro_geral; ?>
                         </div>
                 <?php }?>
                 <input class="input-login" name="nome" placeholder="Nome" type="text">
                 <input class="input-login" name="email" placeholder="E-mail" type="text">
                 <input class="input-login" name="senha" placeholder="Senha" type="password">
-                <input class="btn-login" name="entrar" type="submit" value="Entrar">
+                <input class="btn-login" name="cadastrar" type="submit" value="Cadastrar">
             </form>
         </div>
     </main>
--- index.php
+++ index.php
@@ -24,7 +24,7 @@ function verificaLogin()
             if($sql->execute(array($token,$email,$senha_cript))){
             //Armazenar este token na sessão
             $_SESSION['TOKEN'] = $token;
-            header('location: ../index.php'); ?>
+            header('location: protegida.php'); ?>
             <?php }
     }else{
         $erro_login = "Dados incorretos!";
