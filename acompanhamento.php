68c4bd8ec0ab0e701462c1346e2ae23fbd0a32f3
Gustavo Santos <guuhsrdeker@gmail.com>
2023-01-26 00:59:58 -0300

    Header Menu finalizado. Organização das pastas. Gráficos personalizados.

--- acompanhamento.php
+++ acompanhamento.php
@@ -14,27 +14,37 @@ sessionVerif();
     <link rel="stylesheet" href="css/suporte.css">
     <title>Próturbo :: Acompanhamento</title>
     <style>
-        .corpo{
+        .corpo {
             display: flex;
             flex-wrap: wrap;
             justify-content: center;
+            max-width: 800px;
+            overflow: auto;
+            margin-left: auto;
+            margin-right: auto;
+            background-color: whitesmoke;
+            overflow: auto;
         }
-        #refugo, #producao{
-        }
-        text{
-            font-size: 20pt !important;
+
+        #refugo,
+        #producao {
+            margin: 10px;   
+            padding: 10px;
+            max-height: 400px;
+            box-shadow: 0px 0px 10px #cecece;
+            overflow: auto;
         }
     </style>
 </head>
 
 <body>
     <?php include_once("header.php") ?>
-    <h1>Página em desenvolvimento...</h1>
+    <h1>Gráficos</h1>
 
     <div class="corpo">
-        <div id="refugo" style="max-width: 300px; height: 500px;"></div>
+        <div id="refugo" style="width: 320px;"></div>
 
-        <div id="producao" style="max-width: 300px; height: 500px;"></div>
+        <div id="producao" style="width: 320px;"></div>
     </div>
 
 </body>
@@ -43,7 +53,9 @@ sessionVerif();
 
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <script type="text/javascript">
-    google.charts.load('current', { 'packages': ['bar'] });
+    google.charts.load('current', {
+        'packages': ['bar']
+    });
     google.charts.setOnLoadCallback(drawChart);
 
     function drawChart() {
@@ -53,19 +65,45 @@ sessionVerif();
             $sql = $pdo->prepare("SELECT DISTINCT gr_machine, SUM(gr_value) AS gr_value FROM gr 
             GROUP BY gr_machine ORDER BY SUM(gr_value)");
             $sql->execute();
-            while ($row = $sql->fetch(PDO::FETCH_ASSOC)) { ?>
-                ['<?php echo $row['gr_machine'] ?>', '<?php echo $row['gr_value'] ?>'],
+            while ($row = $sql->fetch(PDO::FETCH_ASSOC)) { ?>['<?php echo $row['gr_machine'] ?>', '<?php echo $row['gr_value'] ?>'],
             <?php } ?>
 
         ]);
 
         var options = {
             chart: {
-                title: 'Refugo',
-                subtitle: 'Valores do turno atual',
+                title: 'Refugo'
             },
-            legend: {position: "none"}
+            bars: 'horizontal', // Required for Material Bar Charts.
+            legend: {
+                position: "none"
+            },
+            animation: {
+                duration: 1000,
+                easing: 'out',
+                startup: true
+            },
+            vAxis: {
+                title: ''
+            },
+            titleTextStyle: {
+                fontSize: 20
+            },
+            colors: ['#004479'],
+            backgroundColor: {
+                fill: 'transparent',
+            },
+            hAxis: {
+                gridlines: {
+                    count: 0
+                }
+            },
+            chartArea: {
+                backgroundColor: 'transparent',
+            },
+
         };
+
 
         var chart = new google.charts.Bar(document.getElementById('refugo'));
 
@@ -74,32 +112,59 @@ sessionVerif();
 </script>
 
 <script type="text/javascript">
-google.charts.load('current', {'packages':['bar']});
-      google.charts.setOnLoadCallback(drawChart);
+    google.charts.load('current', {
+        'packages': ['bar']
+    });
+    google.charts.setOnLoadCallback(drawChart);
 
-      function drawChart() {
+    function drawChart() {
         var data = google.visualization.arrayToDataTable([
-          ['Máquina', 'Quantidade'],
-          <?php
+            ['Máquina', 'Quantidade'],
+            <?php
             $sql = $pdo->prepare("SELECT DISTINCT gp_machine, SUM(gp_value) AS gp_value FROM gp 
             GROUP BY gp_machine ORDER BY SUM(gp_value)");
             $sql->execute();
-            while ($row = $sql->fetch(PDO::FETCH_ASSOC)) { ?>
-                ['<?php echo $row['gp_machine'] ?>', '<?php echo $row['gp_value'] ?>'],
+            while ($row = $sql->fetch(PDO::FETCH_ASSOC)) { ?>['<?php echo $row['gp_machine'] ?>', '<?php echo $row['gp_value'] ?>'],
             <?php } ?>
         ]);
 
         var options = {
-          chart: {
-            title: 'Produção',
-            subtitle: 'Valores do turno',
-          },
-          bars: 'horizontal', // Required for Material Bar Charts.
-          legend: {position: "none"}
+            chart: {
+                title: 'Produção'
+            },
+            bars: 'horizontal', // Required for Material Bar Charts.
+            legend: {
+                position: "none"
+            },
+            animation: {
+                duration: 1000,
+                easing: 'out',
+                startup: true
+            },
+            vAxis: {
+                title: ''
+            },
+            titleTextStyle: {
+                fontSize: 20
+            },
+            colors: ['#004479'],
+            backgroundColor: {
+                fill: 'transparent',
+            },
+            hAxis: {
+                gridlines: {
+                    count: 0
+                }
+            },
+            chartArea: {
+                backgroundColor: 'transparent',
+            },
+
         };
+
 
         var chart = new google.charts.Bar(document.getElementById('producao'));
 
         chart.draw(data, google.charts.Bar.convertOptions(options));
-      }
+    }
 </script>
--- header.php
+++ header.php
@@ -1,34 +1,45 @@
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="css/header.css">
 <link rel="stylesheet" href="css/animate.css"> <!--Animações-->
 <link rel="stylesheet" href="css/hover.css"> <!--Animações-->
 <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-<header>
-    <nav>
+<script defer src="js/header.js"></script>
+<style>
+    /* Adicionando a classe CSS para esconder o menu */
+    .nav-menu {
+        display: none;
+    }
+
+    /* Adicionando a classe CSS para mostrar o menu */
+    .nav-menu-open {
+        display: block;
+    }
+</style>
+<header class="header">
+    <a class="logo" href="refugo.php">Início</a>
+    <nav class="nav">
+        <button class="btn-menu" aria-label="Abrir Menu" aria-haspopup="true" aria-controls="menu" aria-expanded="false">
+            Menu<span class="hamburger"></span>
+        </button>
         <?php
         global $user;
         $user = auth($_SESSION['TOKEN']);
         if ($user) {
             if ($user['user_level'] == 'admin') { ?>
                 <!-- <p><php echo "Administrador: ".$user['user_name'] ?></p> -->
-                <a class="logo" href="refugo.php">Início</a>
-                <div class="mobile-menu">
-                    <div class="line1"></div>
-                    <div class="line2"></div>
-                    <div class="line3"></div>
-                </div>
-                <ul class="nav-list">
-                <a class='a-menu hvr-grow' id='botaologin' href='acompanhamento.php'>Acompanhameto</a>
-                <a class='a-menu hvr-grow' id='botaologin' href='historico.php'>Histórico</a>
-                <a class='a-menu hvr-grow' id='botaologin' href='suporte.php'>Suporte</a>
-                <a class='a-menu hvr-grow sair' id='botaologin' href='logout.php'>Sair</a>
+
+                <ul class="menu" id="menu" role="menu">
+                    <a id='botaologin' href='acompanhamento.php'>Acompanhameto</a>
+                    <a id='botaologin' href='historico.php'>Histórico</a>
+                    <a id='botaologin' href='suporte.php'>Suporte</a>
+                    <a id='botaologin' href='logout.php'>Sair</a>
                 </ul>
             <?php } else { ?>
-                <a class='logouser hvr-grow' href='refugo.php'><img class="imguser" src="img/logo.png" alt=""></a>
-                <a class='a-menu hvr-grow' id='botaologin' href='historico.php'>Histórico</a>
-                <a class='a-menu hvr-grow sair' id='botaologin' href='logout.php'>Sair</a>
-            <?php }
+                <ul class="menu" id="menu" role="menu">
+                    <a id='botaologin' href='historico.php'>Histórico</a>
+                    <a id='botaologin' href='logout.php'>Sair</a>
+                </ul>
+        <?php }
         }
         ?>
     </nav>
 </header>
-<script src="mobile-navbar.js"></script>
