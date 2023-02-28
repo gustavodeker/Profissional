3c13f39290017a4709ccde9bf88f6d87ce14768c
Gustavo Santos <guuhsrdeker@gmail.com>
2023-02-28 07:16:57 -0300

    Iniciando área de parada

---
+++ Proturbo/Web/parada.php
@@ -0,0 +1,38 @@
+<?php
+include("config/conexao.php");
+sessionVerif();
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Parada</title>
+    <link rel="stylesheet" href="css/geral.css">
+    <link rel="stylesheet" href="css/header.css">
+</head>
+
+<body>
+    <?php include("header.php") ?>
+
+    <div class="divCorpo">
+        <form action="" method="post">
+            <select name="" id=""></select>
+
+            <input type="text">
+
+            <input type="datetime-local">
+
+            <input type="datetime-local">
+
+            <input type="text">
+        </form>
+    </div>
+
+    <?php include('footer.php'); ?>
+</body>
+
+</html>
